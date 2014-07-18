a6999cae3fcb76ab7b63c2a594c6d8a3225323f3
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2014-07-18 14:14:37 +0200

    Weird conflict resolution

--- cameo/solver_based_model.py
+++ cameo/solver_based_model.py
@@ -525,7 +525,7 @@ class SolverBasedModel(Model):
                 time_machine.reset()
         return essential
 
-    #TODO: describe the formats in doc
+    # TODO: describe the formats in doc
     def load_medium(self, medium, copy=False):
         """
         Loads a medium into the model. If copy is true it will return
@@ -572,7 +572,7 @@ class SolverBasedModel(Model):
 
     @staticmethod
     def _load_medium_from_dataframe(model, medium):
-        for i in xrange(len(medium) -1):
+        for i in xrange(len(medium) - 1):
             rid = medium['reaction_id'][i]
             if model.reactions.has_id(rid):
                 model.reactions.get_by_id(rid).lower_bound = medium['lower_bound'][i]
