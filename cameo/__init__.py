6f5985c4b861ff841d016dfba9a2d374c6dcfe05
Joao Cardoso <joaca@biosustain.dtu.dk>
2014-09-10 16:10:17 +0200

    sanitize_code

--- cameo/__init__.py
+++ cameo/__init__.py
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+ID_SANITIZE_RULES = [('_DASH_', '__'), ('_FSLASH_', '/'),('_BSLASH_', "\\"), ('_LPAREN_', '('), ('_LSQBKT_', '['),
+                     ('_RSQBKT_', ']'), ('_RPAREN_', ')'), ('_COMMA_', ','), ('_PERIOD_', '.'), ('_APOS_', "'"),
+                     ('&amp;', '&'), ('&lt;', '<'), ('&gt;', '>'), ('&quot;', '"'), ('__', '-')]
+
 
 #TODO: describe cameo with some examples
 """
@@ -33,3 +37,26 @@ from .flux_analysis.analysis import flux_variability_analysis, phenotypic_phase_
 from .flux_analysis.simulation import fba, pfba
 
 import config
+
+
+def sanitize_ids(model):
+    for metabolite in model.metabolites:
+        metabolite.id = _apply_sanitize_rules(metabolite.id)
+        metabolite.name = _apply_sanitize_rules(metabolite.name)
+
+    for reaction in model.reactions:
+        reaction.id = _apply_sanitize_rules(reaction.id)
+        reaction.name = _apply_sanitize_rules(reaction.name)
+
+    for variable in model.solver.variables:
+        variable.name = _apply_sanitize_rules(variable.name)
+
+    for constraint in model.solver.constraints:
+        constraint.name = _apply_sanitize_rules(constraint.name)
+
+
+def _apply_sanitize_rules(id):
+    for rule in ID_SANITIZE_RULES:
+        id = id.replace(*rule)
+
+    return id
