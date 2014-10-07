5473fac863973792c4d53082eef0836fa1b61775
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2014-10-07 17:07:36 +0200

    Updated optlang version (fixes an issue with primal rounding). cobrapy pfba test passes now.

--- tests/test_cobrapy_compatibility.py
+++ tests/test_cobrapy_compatibility.py
@@ -20,34 +20,3 @@ for cls in (CobraTestCase, TestReactions, TestCobraFluxAnalysis):
 
 if __name__ == '__main__':
     nose.runmodule()
-
-    # Debug ...
-    # from cobra.flux_analysis.variability import flux_variability_analysis
-    # from cobra.flux_analysis.single_deletion import single_deletion, single_gene_deletion
-    # from cobra.manipulation.modify import initialize_growth_medium
-    # solver = GlpkModel()
-    # cobra_model = to_solver_based_model(create_test_model(test_pickle), solver)
-    # cobra_model.media_compositions['lb']
-    # print cobra_model.optimize().f
-    # print 1
-    # initialize_growth_medium(cobra_model, 'LB')
-    # print cobra_model.optimize().f
-    # print 2
-    # the_loci =  ['STM4081', 'STM0247', 'STM3867', 'STM2952']
-    # the_genes = tpiA, metN, atpA, eno = map(cobra_model.genes.get_by_id, the_loci)
-    # id_to_name = dict([(x.id, x.name) for x in the_genes])
-    # growth_dict = {'fba':{tpiA.id:2.41, metN.id:2.44, atpA.id:1.87, eno.id:1.81},
-    #                'moma':{ tpiA.id:1.62, metN.id:2.4, atpA.id:1.40, eno.id:0.33}}
-
-
-    # element_list = the_loci
-    # rates, statuses, problems = single_gene_deletion(cobra_model, element_list=element_list, method='fba')
-
-    # cobra_model.reactions.get_by_id('biomass_iRR1083_metals').lower_bound = 2.4620629521939827
-    # the_problem = 'return'
-    # fva_out = flux_variability_analysis(cobra_model,
-    #                                             the_problem=the_problem,
-    # the_reactions=cobra_model.reactions[100:140])
-
-    # objective_metabolite = Metabolite('objective')
-    #
