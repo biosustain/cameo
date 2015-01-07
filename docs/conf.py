6040b75b95f018757e8ed8235889c82ef394319d
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2015-01-07 20:23:36 +0100

    Back to old RTD requirements. Mocking sympy in conf.py instead.

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ if on_rtd:
         def __getattr__(cls, name):
             return Mock()
 
-    MOCK_MODULES = ['numpy', 'pandas', 'scipy', 'bokeh', 'swiglpk']
+    MOCK_MODULES = ['numpy', 'pandas', 'scipy', 'bokeh', 'swiglpk', 'sympy']
     sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
