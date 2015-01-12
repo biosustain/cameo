b7ba8b30db15c4202fa174cc9947604f8c822ed5
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2015-01-12 11:36:44 +0100

    Added data to MANIFEST.in

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,8 @@ if on_rtd:
         def __getattr__(cls, name):
             return Mock()
 
-    MOCK_MODULES = ['numpy', 'numpy.random', 'matplotlib', 'pandas', 'scipy', 'scipy.sparse', 'scipy.io', 'scipy.stats', 'scipy.version', 'bokeh', 'swiglpk',
+    MOCK_MODULES = ['numpy', 'numpy.random', 'matplotlib', 'pandas', 'scipy', 'scipy.sparse',
+                    'scipy.io', 'scipy.stats', 'scipy.version', 'bokeh', 'bokeh.plotting', 'swiglpk',
                     'glpk', 'gurobipy', 'gurobipy.GRB', 'cplex', 'mlabwrap', 'pp', 'libsbml']
     sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
