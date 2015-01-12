50fe25d2f4230089091b5843d746d68ff1d3d976
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2015-01-12 11:24:53 +0100

    Putting optlang back in RTD requirements.

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ if on_rtd:
             return Mock()
 
     MOCK_MODULES = ['numpy', 'numpy.random', 'pandas', 'scipy', 'scipy.sparse', 'scipy.io', 'scipy.stats', 'scipy.version', 'bokeh', 'swiglpk',
-                    'optlang', 'glpk', 'gurobipy', 'gurobipy.GRB', 'cplex', 'mlabwrap', 'pp', 'libsbml']
+                    'glpk', 'gurobipy', 'gurobipy.GRB', 'cplex', 'mlabwrap', 'pp', 'libsbml']
     sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
