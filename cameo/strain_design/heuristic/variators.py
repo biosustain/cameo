5831a77c20ad7c58c1f627301c6ad62b09f8bf78
Joao Cardoso <joaca@biosustain.dtu.dk>
2015-05-18 14:57:51 +0200

    Give int representation as arg on the internal method .

--- cameo/strain_design/heuristic/variators.py
+++ cameo/strain_design/heuristic/variators.py
@@ -25,13 +25,13 @@ from cameo.strain_design.heuristic.genomes import MultipleChromosomeGenome
 from numpy import float32 as float
 
 
-def _do_set_n_point_crossover(representation, mom, dad, points):
+def _do_set_n_point_crossover(int_representation, mom, dad, points):
     chunks = []
     i = 0
     for point in points:
-        chunks.append([representation.index(v) for v in representation[i:point]])
+        chunks.append(int_representation[i:point])
         i = point
-    chunks.append([representation.index(v) for v in representation[i:]])
+    chunks.append(int_representation[i:])
 
     bro = OrderedSet()
     sis = OrderedSet()
@@ -50,12 +50,13 @@ def _do_set_n_point_crossover(representation, mom, dad, points):
 @crossover
 def set_n_point_crossover(random, mom, dad, args):
     representation = args.get('representation')
+    int_representation = [representation.index(v) for v in representation]
     crossover_rate = args.setdefault('crossover_rate', 1.0)
     num_crossover_points = args.setdefault('num_crossover_points', 1)
     children = []
     if random.random() < crossover_rate:
-        points = random.sample(representation, num_crossover_points)
-        bro, sis = _do_set_n_point_crossover(representation, mom, dad, points)
+        points = random.sample(int_representation, num_crossover_points)
+        bro, sis = _do_set_n_point_crossover(int_representation, mom, dad, points)
 
         children.append(bro)
         children.append(sis)
--- tests/test_strain_design_heuristics.py
+++ tests/test_strain_design_heuristics.py
@@ -578,10 +578,11 @@ class TestReactionKnockoutOptimization(unittest.TestCase):
 class TestVariators(unittest.TestCase):
     def test_set_n_point_crossover(self):
         representation = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"]
+        int_representation = [representation.index(v) for v in representation]
         mom = OrderedSet([representation.index(v) for v in ["A", "B", "E", "K", "L", "M"]])
         dad = OrderedSet([representation.index(v) for v in ["A", "C", "I", "J", "K", "L"]])
         points = [4]
-        children = _do_set_n_point_crossover(representation, mom, dad, points)
+        children = _do_set_n_point_crossover(int_representation, mom, dad, points)
         bro = OrderedSet([0, 1, 8, 9, 10, 11])
         sis = OrderedSet([0, 2, 4, 10, 11, 12])
         self.assertEqual(children[0], bro)
