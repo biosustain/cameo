40759d85a3809851f677bf8e9d7cd5d3e8251f53
João Cardoso <jcardoso@silicolife.com>
2015-06-29 23:23:13 +0200

    py3 compatible base64 encode with six if PY2 PY3

--- cameo/ui/__init__.py
+++ cameo/ui/__init__.py
@@ -29,12 +29,19 @@ ASSETS = os.path.join(os.path.dirname(__file__), "assets")
 
 SEARCHING_IMAGE_FILE = os.path.join(ASSETS, "searching.gif")
 with open(SEARCHING_IMAGE_FILE, "rb") as f:
-    SEARCHING_IMAGE = b64encode(six.b(f.read())).replace('\n', '')
+    if six.PY2:
+        SEARCHING_IMAGE = b64encode(f.read()).replace('\n', '')
+    elif six.PY3:
+        SEARCHING_IMAGE = b64encode(bytearray(f.read())).replace('\n', '')
+
 
 
 LOADING_IMAGE_FILE = os.path.join(ASSETS, "searching.gif")
 with open(SEARCHING_IMAGE_FILE, "rb") as f:
-    LOADING_IMAGE = b64encode(six.b(f.read())).replace('\n', '')
+    if six.PY2:
+        LOADING_IMAGE = b64encode(f.read()).replace('\n', '')
+    elif six.PY3:
+        LOADING_IMAGE = b64encode(bytearray(f.read())).replace('\n', '')
 
 
 logger = logging.getLogger(__name__)
