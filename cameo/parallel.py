c695c80b55d207117e06a4c4144419985161cfb6
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2015-07-29 21:40:42 +0200

    Make SequentialView py 3 compatible
    There is no itertools.imap in Python 3. Use six.moves.map instead.

--- cameo/parallel.py
+++ cameo/parallel.py
@@ -16,7 +16,6 @@ from __future__ import absolute_import, print_function
 
 import six.moves.queue
 from multiprocessing import Pool, cpu_count
-import itertools
 from cameo.util import Singleton
 
 import logging
@@ -235,7 +234,7 @@ class SequentialView(object):
         return func(*args, **kwargs)
 
     def imap(self, func, *args, **kwargs):
-        return itertools.imap(func, *args, **kwargs)
+        return map(func, *args, **kwargs)
 
     def __len__(self):
         return 1
