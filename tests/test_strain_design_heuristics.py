63c00f73a663c8a5afd8a596dc773742dc35552e
Joao Cardoso <joaca@biosustain.dtu.dk>
2015-08-04 10:09:29 +0200

    python is wierd!

--- tests/test_strain_design_heuristics.py
+++ tests/test_strain_design_heuristics.py
@@ -625,7 +625,11 @@ class TestReactionKnockoutOptimization(unittest.TestCase):
                                            objective_function=objective,
                                            seed=SEED)
 
+        self.assertEqual(rko.random.random(), 0.1915194503788923)
+
         results = rko.run(max_evaluations=3000, pop_size=10, view=SequentialView())
+
+        self.assertEqual(rko.random.random(), 0.9268454219291495)
 
         with open(result_file, 'rb') as in_file:
             expected_results = pickle.load(in_file)
@@ -648,7 +652,11 @@ class TestReactionKnockoutOptimization(unittest.TestCase):
                                            heuristic_method=inspyred.ec.emo.NSGA2,
                                            seed=SEED)
 
+        self.assertEqual(rko.random.random(), 0.1915194503788923)
+
         results = rko.run(max_evaluations=3000, pop_size=10, view=SequentialView())
+
+        print(rko.random.random(), 0.545818634701)
 
         with open(result_file, 'rb') as in_file:
             expected_results = pickle.load(in_file)
