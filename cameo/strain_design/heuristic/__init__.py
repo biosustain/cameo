fb52b515c0c0f9a01cd66c5a6edfda010b465009
Joao Cardoso <joaca@biosustain.dtu.dk>
2016-01-07 11:43:33 +0100

    cameo.strain_design.heuristic provides full developed methods only on autocomplete.

--- cameo/strain_design/heuristic/__init__.py
+++ cameo/strain_design/heuristic/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .optimization import ReactionKnockoutOptimization, GeneKnockoutOptimization
+from evolutionary_based import OptGene
