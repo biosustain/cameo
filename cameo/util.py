b24b78d925ebc15e6ec069c9cbe52e2307b49866
Joao Cardoso <joaca@biosustain.dtu.dk>
2016-02-23 15:08:36 +0100

    fix: singleton is py3 compatible

--- cameo/util.py
+++ cameo/util.py
@@ -234,7 +234,7 @@ class Singleton(object):
 
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
-            cls._instance = super(Singleton, cls).__new__(cls, *args, **kwargs)
+            cls._instance = super(Singleton, cls).__new__(cls)
         return cls._instance
 
 
