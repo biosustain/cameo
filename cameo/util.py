85630fd09535988ab5a22fbfbc5ba1c722c87fd6
Joao Cardoso <joaca@biosustain.dtu.dk>
2016-02-23 15:53:33 +0100

    feat: frozendict implementation

--- cameo/util.py
+++ cameo/util.py
@@ -40,6 +40,32 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+class frozendict(dict):
+    def __init__(self, iterable, **kwargs):
+        super(frozendict, self).__init__(iterable, **kwargs)
+
+    def popitem(self):
+        raise AttributeError("'frozendict' object has no attribute 'popitem")
+
+    def pop(self, k, d=None):
+        raise AttributeError("'frozendict' object has no attribute 'pop")
+
+    def __setitem__(self, key, value):
+        raise AttributeError("'frozendict' object has no attribute '__setitem__")
+
+    def setdefault(self, k, d=None):
+        raise AttributeError("'frozendict' object has no attribute 'setdefault")
+
+    def __delitem__(self, key):
+        raise AttributeError("'frozendict' object has no attribute '__delitem__")
+
+    def __hash__(self):
+        return hash(tuple(sorted(self.items())))
+
+    def update(self, E=None, **F):
+        raise AttributeError("'frozendict' object has no attribute 'update")
+
+
 class ProblemCache(object):
     """
     Variable and constraint cache for models.
