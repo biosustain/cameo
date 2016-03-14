dda3b16dc8ef836bfd67d5239f4c189199d25b5d
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2016-03-14 14:16:49 +0100

    doc: write more docs

--- docs/conf.py
+++ docs/conf.py
@@ -58,8 +58,11 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx'
 ]
+
+intersphinx_mapping = {'cobrapy': ('https://cobrapy.readthedocs.org/en/latest/', None)}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
