d1f5069057bcf008414299393890104b312a4421
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2016-07-25 13:37:06 +0200

    chore: update cobrapy compatibility tests

--- tests/test_cobrapy_compatibility.py
+++ tests/test_cobrapy_compatibility.py
@@ -31,5 +31,4 @@ for cls in (CobraTestCase, TestReactions, TestCobraFluxAnalysis):
     cls.setUp = types.MethodType(setUp, cls)
 
 del TestReactions.testGPR
-del TestCobraFluxAnalysis.test_single_gene_deletion
 del TestCobraFluxAnalysis.test_phenotype_phase_plane  # Avoid bug in cobra tests (AttributeError on self.skip() )
