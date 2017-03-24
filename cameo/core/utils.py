fee0030d390d8e6e4cc0ef06a7dfa62581296628
Henning Redestig <henning.red@gmail.com>
2017-03-24 17:50:15 +0100

    refactor: move metabolite.knock_out add_exchange
    Move these to cobra. metabolite.knock_out must be metabolite method to
    enable target.knock_out symmetry. Depends on add_exchange

--- cameo/core/utils.py
+++ cameo/core/utils.py
@@ -1,36 +1,4 @@
-import cameo
-
-
-def add_exchange(model, metabolite, demand=True, prefix='DM_', bound=1000.0):
-    """Add an exchange reaction for a metabolite (demand=TRUE: metabolite --> Ø or demand=False: 0 --> metabolite )
-
-    Parameters
-    ----------
-    model : cameo.core.SolverBasedModel
-        The model to add the exchange reaction to.
-    metabolite : cameo.core.Metabolite
-    demand : bool, optional
-        True for sink type exchange, False for uptake type exchange
-    prefix : str, optional
-        A prefix that will be added to the metabolite ID to be used as the demand reaction's ID (defaults to 'DM_').
-    bound : float, optional
-        Upper bound for sink reaction / lower bound for uptake (multiplied by -1)
-
-    Returns
-    -------
-    Reaction
-        The created exchange reaction.
-    """
-    reaction_id = str(prefix + metabolite.id)
-    name = "Exchange %s" % metabolite.name if prefix != "DM_" else "Demand %s" % metabolite.name
-    if reaction_id in model.reactions:
-        raise ValueError("The metabolite already has a demand reaction.")
-
-    reaction = cameo.core.Reaction(id=reaction_id, name=name)
-    reaction.add_metabolites({metabolite: -1})
-    reaction.bounds = (0, bound) if demand else (-bound, 0)
-    model.add_reactions([reaction])
-    return reaction
+from cobra.util import add_exchange
 
 
 def get_reaction_for(model, value, add=True):
