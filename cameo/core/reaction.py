9293ef89fcd1670a84306b473632173d020496d3
Henning Redestig <henning.red@gmail.com>
2017-03-27 09:26:11 +0200

    refactor: remove n_carbon
    Newly introduced but hardly used attribute for metabolite and
    reaction. Remove it in favor of considering more general property.

--- cameo/core/reaction.py
+++ cameo/core/reaction.py
@@ -458,16 +458,16 @@ class Reaction(cobra.core.Reaction):
     #     if ub is not None:
     #         self.upper_bound = ub
 
-    @property
-    def n_carbon(self):
-        """number of carbon atoms
-
-        Returns
-        -------
-        int
-           number of carbons for all metabolites involved in a reaction
-        """
-        return sum(metabolite.n_carbon for metabolite in self.metabolites)
+    # @property
+    # def n_carbon(self):
+    #     """number of carbon atoms
+    #
+    #     Returns
+    #     -------
+    #     int
+    #        number of carbons for all metabolites involved in a reaction
+    #     """
+    #     return sum(metabolite.n_carbon for metabolite in self.metabolites)
 
     def _repr_html_(self):
         return """
