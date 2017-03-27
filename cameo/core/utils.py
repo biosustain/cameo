771e0d3daa0ef9e2f7d3db3c16e3453f41d38588
Henning Redestig <henning.red@gmail.com>
2017-03-27 09:54:07 +0200

    refactor: move medium definitions
    In favor of cobrapy's medium definition.

--- cameo/core/utils.py
+++ cameo/core/utils.py
@@ -1,4 +1,8 @@
+import six
+import csv
+
 from cobra.util import add_exchange
+from pandas import DataFrame
 
 
 def get_reaction_for(model, value, add=True):
@@ -37,3 +41,95 @@ def get_reaction_for(model, value, add=True):
             else:
                 raise KeyError('Invalid target %s' % value)
     return reactions[0]
+
+
+def medium(model):
+    """Current medium for this model."""
+    reaction_ids = []
+    reaction_names = []
+    lower_bounds = []
+    upper_bounds = []
+    for ex in model.exchanges:
+        metabolite = list(ex.metabolites.keys())[0]
+        coeff = ex.metabolites[metabolite]
+        if coeff * ex.lower_bound > 0:
+            reaction_ids.append(ex.id)
+            reaction_names.append(ex.name)
+            lower_bounds.append(ex.lower_bound)
+            upper_bounds.append(ex.upper_bound)
+
+    return DataFrame({'reaction_id': reaction_ids,
+                      'reaction_name': reaction_names,
+                      'lower_bound': lower_bounds,
+                      'upper_bound': upper_bounds},
+                     index=None, columns=['reaction_id', 'reaction_name', 'lower_bound', 'upper_bound'])
+
+
+def load_medium(model, medium_def, copy=False, delimiter="\t"):
+    """
+    Loads a medium into the model. If copy is true it will return
+    a copy of the model. Otherwise it applies the medium to itself.
+    Supported formats
+    TODO
+
+    Parameters
+    ----------
+    model : cameo.core.SolverBasedModel
+        The model to load medium for
+    medium_def: str, pandas.DataFrame, dict.
+        The medium to load
+    copy: boolean, optional
+        If True copies the model, otherwise the changes will happen inplace.
+    delimiter: str
+        Only if loading the medium from a file.
+
+    Returns
+    -------
+    SolverBasedModel
+        If copy=True, returns a copy of the model.
+
+    """
+
+    if copy:
+        model = model.copy()
+    else:
+        model = model
+    if isinstance(medium_def, dict):
+        _load_medium_from_dict(model, medium_def)
+    elif isinstance(medium_def, DataFrame):
+        _load_medium_from_dataframe(model, medium_def)
+    elif isinstance(medium_def, six.string_types):
+        _load_medium_from_file(model, medium_def, delimiter=delimiter)
+    else:
+        raise AssertionError("input type (%s) is not valid" % type(medium))
+
+    return model
+
+
+def _load_medium_from_dict(model, medium_def):
+    assert isinstance(medium_def, dict)
+    for ex_reaction in model.exchanges:
+        ex_reaction.lower_bound = medium_def.get(ex_reaction.id, 0)
+
+
+def _load_medium_from_file(model, file_path, delimiter="\t"):
+    this_medium = {}
+
+    with open(file_path, "rb") as csv_file:
+        reader = csv.reader(csv_file, delimiter=delimiter)
+
+        for row in reader:
+            model.reactions.get_by_id(row[0])
+            this_medium[row[0]] = row[1]
+
+    _load_medium_from_dict(model, this_medium)
+
+
+def _load_medium_from_dataframe(model, medium_df):
+    assert isinstance(medium_df, DataFrame)
+    for ex_reaction in model.exchanges:
+        if ex_reaction.id in medium_df.reaction_id.values:
+            medium_row = medium_df[medium_df.reaction_id == ex_reaction.id]
+            ex_reaction.lower_bound = medium_row.lower_bound.values[0]
+        else:
+            ex_reaction.lower_bound = 0
