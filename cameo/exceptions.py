a862cd9a0a678abdbf13c5f84dc1cbba8d0f9d14
Henning Redestig <henning.red@gmail.com>
2017-03-29 16:22:10 +0200

    refactor: move solver exceptions to cobra
    These exceptions are necessary in cobra as well, so let's define them
    there and cobra's `assert_optimal` to handle which error to throw.

--- cameo/exceptions.py
+++ cameo/exceptions.py
@@ -27,24 +27,3 @@ class IncompatibleTargets(Exception):
 class SolveError(Exception):
     def __init__(self, message):
         super(SolveError, self).__init__(message)
-
-
-class Infeasible(SolveError):
-    pass
-
-
-class Unbounded(SolveError):
-    pass
-
-
-class FeasibleButNotOptimal(SolveError):
-    pass
-
-
-class UndefinedSolution(SolveError):
-    pass
-
-
-_OPTLANG_TO_EXCEPTIONS_DICT = dict((
-    (optlang.interface.INFEASIBLE, Infeasible), (optlang.interface.UNBOUNDED, Unbounded),
-    (optlang.interface.FEASIBLE, FeasibleButNotOptimal), (optlang.interface.UNDEFINED, UndefinedSolution)))
