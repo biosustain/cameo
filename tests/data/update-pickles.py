6b099597b9df7097f0fa8fc9708fd5ba56a73e7f
Henning Redestig <henning.red@gmail.com>
2017-03-30 09:16:19 +0200

    fix: save pickles in protocol 2 / use glpk
    To enable reading in old pythons and with missing cplex

--- tests/data/update-pickles.py
+++ tests/data/update-pickles.py
@@ -1,13 +1,15 @@
 import pickle
 
 import cobra.test
+import optlang
 
 from cameo import load_model
 
-ijo = load_model('iJO1366.xml')
+ijo = load_model('iJO1366.xml', solver_interface=optlang.glpk_interface)
 with open('iJO1366.pickle', 'wb') as out:
-    pickle.dump(ijo, out)
+    pickle.dump(ijo, out, protocol=2)
 
 salmonella = cobra.test.create_test_model('salmonella')
+salmonella.solver = 'glpk'
 with open('salmonella.pickle', 'wb') as out:
-    pickle.dump(salmonella, out)
+    pickle.dump(salmonella, out, protocol=2)
