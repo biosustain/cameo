6826ce258412dde3229cc9e6b54db64cab0bef17
João Gonçalo Rocha Cardoso <jooaaoo@gmail.com>
2018-02-06 12:45:48 +0100

    Fix: publication fixes (#198)
    * fix: update cobra version
    
    * fix: add docstring to the package
    
    To remove any confusion from the reviewers heads.

--- cameo/strain_design/deterministic/linear_programming.py
+++ cameo/strain_design/deterministic/linear_programming.py
@@ -11,6 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""
+This module contains algorithms based on linear programming techniques, including mixed-integer linear programming
+"""
 
 from __future__ import print_function
 
