1f48f6be4812afa7b6974136d9d21a2b53cca2ff
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2018-05-12 00:53:10 +0200

    refactor: modify script for new cplex

--- .travis/install_cplex.sh
+++ .travis/install_cplex.sh
@@ -1,10 +1,14 @@
-curl -L $SECRET_CPLEX_LINK -o cplex.tar.gz
-tar xvf cplex.tar.gz
-if [[ $TRAVIS_PYTHON_VERSION == "3.4" ]]; then
-	cd "cplex/python/3.4/x86-64_linux/";
+#!/usr/bin/env bash
+
+set -exu
+
+# Build on master and tags.
+# $CPLEX_URL is defined in the Travis repository settings.
+if [[ ("${TRAVIS_BRANCH}" == "master" || -n "${TRAVIS_TAG}") \
+&& ("${TRAVIS_PYTHON_VERSION}" == "2.7" || "${TRAVIS_PYTHON_VERSION}" == "3.5") ]];then
+    curl -L "${CPLEX_URL}" -o cplex.tar.gz
+    tar xzf cplex.tar.gz
+    cd "cplex/python/${TRAVIS_PYTHON_VERSION}/x86-64_linux"
+    python setup.py install
+    cd "${TRAVIS_BUILD_DIR}"
 fi
-if [[ $TRAVIS_PYTHON_VERSION == "2.7" ]]; then
-	cd "cplex/python/2.6/x86-64_linux/";
-fi
-pip install .
-cd $TRAVIS_BUILD_DIR
