94ce3c1177b85df3edb4065ef0ff4cca0c94d0db
Svetlana <galkina.s.a@gmail.com>
2018-06-21 15:17:15 +0200

    fix: cplex installation

--- .travis/install_cplex.sh
+++ .travis/install_cplex.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-set -exu
+set -eu
 
 # Build on master and tags.
 # $CPLEX_URL is defined in the Travis repository settings.
 if [[ ("${TRAVIS_BRANCH}" == "master" || -n "${TRAVIS_TAG}") \
 && ("${TRAVIS_PYTHON_VERSION}" == "2.7" || "${TRAVIS_PYTHON_VERSION}" == "3.5") ]];then
-    curl -L "${CPLEX_URL}" -o cplex.tar.gz
-    tar xzf cplex.tar.gz
-    cd "cplex/python/${TRAVIS_PYTHON_VERSION}/x86-64_linux"
+    python ./.travis/load_dependency.py "${GH_TOKEN}" "cplex-python${TRAVIS_PYTHON_VERSION}.tar.gz"
+    tar xzf cplex-python${TRAVIS_PYTHON_VERSION}.tar.gz
+    cd "cplex/python/3.4/x86-64_linux"
     python setup.py install
     cd "${TRAVIS_BUILD_DIR}"
 fi
