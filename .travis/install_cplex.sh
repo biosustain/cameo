cf29bdfdb0eee5b42a8669cbc0115c996c83be0e
Svetlana <galkina.s.a@gmail.com>
2018-06-21 15:35:01 +0200

    fix: cameo for python2.7

--- .travis/install_cplex.sh
+++ .travis/install_cplex.sh
@@ -3,12 +3,17 @@
 set -eu
 
 # Build on master and tags.
-# $CPLEX_URL is defined in the Travis repository settings.
+# $CPLEX_URL and $GH_TOKEN is defined in the Travis repository settings.
 if [[ ("${TRAVIS_BRANCH}" == "master" || -n "${TRAVIS_TAG}") \
 && ("${TRAVIS_PYTHON_VERSION}" == "2.7" || "${TRAVIS_PYTHON_VERSION}" == "3.5") ]];then
     python ./.travis/load_dependency.py "${GH_TOKEN}" "cplex-python${TRAVIS_PYTHON_VERSION}.tar.gz"
     tar xzf cplex-python${TRAVIS_PYTHON_VERSION}.tar.gz
-    cd "cplex/python/3.4/x86-64_linux"
+    PYTHON_VERSION=${TRAVIS_PYTHON_VERSION}
+    if [ "${PYTHON_VERSION}" == "3.5" ]
+    then
+    PYTHON_VERSION="3.4"
+    fi
+    cd "cplex/python/${PYTHON_VERSION}/x86-64_linux"
     python setup.py install
     cd "${TRAVIS_BUILD_DIR}"
 fi
