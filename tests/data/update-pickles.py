9b3f7db636255fc3583078ca9c0d51128ae669ef
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2019-06-19 19:52:06 +0200

    chore: update model pickles

--- tests/data/update-pickles.py
+++ tests/data/update-pickles.py
@@ -1,15 +1,20 @@
 import pickle
 
-import cobra.test
-import optlang
+import cobra
+from cobra.test import create_test_model
+from optlang import glpk_interface
 
 from cameo import load_model
 
-ijo = load_model('iJO1366.xml', solver_interface=optlang.glpk_interface)
+
+config = cobra.Configuration()
+config.solver = "glpk"
+
+
+ijo = load_model('iJO1366.xml', glpk_interface)
 with open('iJO1366.pickle', 'wb') as out:
     pickle.dump(ijo, out, protocol=2)
 
-salmonella = cobra.test.create_test_model('salmonella')
-salmonella.solver = 'glpk'
+salmonella = create_test_model('salmonella')
 with open('salmonella.pickle', 'wb') as out:
     pickle.dump(salmonella, out, protocol=2)
