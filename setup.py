3e90ff7d7c2b3763de16c2f61aaa99d7fdad95a0
carrascomj <carrascomurielj@gmail.com>
2020-10-20 16:17:25 +0200

    docs: remove support for py35

--- setup.py
+++ setup.py
@@ -85,7 +85,6 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Topic :: Utilities',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
