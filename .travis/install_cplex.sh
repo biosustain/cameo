034cd35b1c3baf7abcdfa3c91b3091a0e7882bc6
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2020-11-06 14:37:10 +0100

    fix: enable cplex on py3.6 on travis-ci

--- .travis/install_cplex.sh
+++ .travis/install_cplex.sh
@@ -3,18 +3,15 @@
 set -eu
 
 # Build on master and tags.
-if [[ ("${TRAVIS_BRANCH}" == "master" || -n "${TRAVIS_TAG}") \
-&& ("${TRAVIS_PYTHON_VERSION}" == "2.7" || "${TRAVIS_PYTHON_VERSION}" == "3.5") ]];then
+# if [[ ("${TRAVIS_BRANCH}" == "master" || -n "${TRAVIS_TAG}") \
+# && ("${TRAVIS_PYTHON_VERSION}" == "2.7" || "${TRAVIS_PYTHON_VERSION}" == "3.5") ]];then
+
+# Build on python 3.6 
+if [[ "${TRAVIS_PYTHON_VERSION}" == "3.6" ]];then
     curl -O $CPLEX_SECRET  # check lastpass
     tar xzf cplex-python3.6.tar.gz
     PYTHON_VERSION=${TRAVIS_PYTHON_VERSION}
-    if [ "${PYTHON_VERSION}" == "3.5" ]
-    then
-    PYTHON_VERSION="3.4"
-    else
-    PYTHON_VERSION="2.6"
-    fi
     cd "cplex/python/${PYTHON_VERSION}/x86-64_linux"
     python setup.py install
-    cd "${TRAVIS_BUILD_DIR}"
+    cd "${TRAVIS_BUILD_DIR}"    
 fi
