7ecd9f6f5bf100736eb9354599f2dcb98279fb6a
carrascomj <carrascomurielj@gmail.com>
2021-01-21 12:13:27 +0100

    chore: install CPLEX on CI py3.6

---
+++ scripts/install_cplex.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -eu
+
+# Build on master and tags.
+if [[ "${PYTHON_VERSION}" == "3.6") ]];then
+    # this should not be logged by GH actions by make it -s silent just in case
+    curl -s -o cplex.zip -L $CPLEX_SECRET 
+    unzip -q cplex.zip
+    ROOT_DIR=`pwd`
+    cd "python/${PYTHON_VERSION}/x86-64_linux"
+    python setup.py install
+    cd $ROOT_DIR
+fi
