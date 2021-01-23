9a4a31dcf83c626842e2ad69142a8d8d9d3665b4
carrascomj <carrascomurielj@gmail.com>
2021-01-23 12:26:01 +0100

    fix: extract cplex properly

--- scripts/install_cplex.sh
+++ scripts/install_cplex.sh
@@ -5,10 +5,10 @@ set -eu
 # Build on master and tags.
 if [[ "${PYTHON_VERSION}" == "3.6" ]];then
     # this should not be logged by GH actions by make it -s silent just in case
-    curl -s -o cplex.zip -L $CPLEX_SECRET 
-    unzip -q cplex.zip
+    curl -s -o cplex36.tar.gz -L $CPLEX_SECRET 
+    tar xzf cplex36.tar.gz
     ROOT_DIR=`pwd`
-    cd "python/${PYTHON_VERSION}/x86-64_linux"
+    cd "cplex_128/python/${PYTHON_VERSION}/x86-64_linux"
     python setup.py install
     cd $ROOT_DIR
 fi
