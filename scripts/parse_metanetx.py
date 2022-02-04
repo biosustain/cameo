7f757eced193a56504e4786dbd6a032801fde002
Sruti <sruti.dammalapati@lanzatech.com>
2022-02-04 14:14:46 -0600

    fix metanetx parser and rebuild universal models

--- scripts/parse_metanetx.py
+++ scripts/parse_metanetx.py
@@ -23,13 +23,12 @@ import requests
 from cobra.core.formula import Formula
 from cobra.io.json import save_json_model
 from pandas import read_table, notnull
+import numpy as np
 
 from cobra import Reaction, Metabolite, Model
 from cameo.io import _apply_sanitize_rules, ID_SANITIZE_RULES_TAB_COMPLETION, ID_SANITIZE_RULES_SIMPHENY
 
 logger = logging.getLogger('parse_metanetx')
-
-
 # logger.setLevel(logging.DEBUG)
 
 
@@ -45,7 +44,9 @@ def parse_reaction(formula, irrev_arrow='-->', rev_arrow='<=>'):
 
         def parse_coeff_and_metabolites(term):
             try:
-                coeff, metabolite_id = term.strip().split(' ')
+                coeff, metabolite_id = term.strip().split(' ')               
+                #dropping compartment
+                metabolite_id = metabolite_id.split('@')[0] 
             except:
                 raise ValueError('Something is fishy with the provided term %s' % term)
             return Metabolite(metabolite_id), factor * float(coeff)
@@ -85,28 +86,32 @@ def construct_universal_model(list_of_db_prefixes, reac_xref, reac_prop, chem_pr
     mnx_reaction_id_selection = set()
     for db_prefix in list_of_db_prefixes:
         mnx_reaction_id_selection.update(reac_xref[reac_xref.XREF.str.startswith(db_prefix)].MNX_ID)
-
+    
+    if np.nan in mnx_reaction_id_selection:
+        mnx_reaction_id_selection.remove(np.nan)
     reaction_selection = reac_prop.loc[mnx_reaction_id_selection]
     reactions = list()
     for index, row in reaction_selection.iterrows():
         try:
-            stoichiometry = parse_reaction(row.Equation, rev_arrow='=')
+            stoichiometry = parse_reaction(row.mnx_equation, rev_arrow='=')
         except ValueError:
             continue
         else:
             for met, coeff in stoichiometry.items():
-                met.name = chem_prop.loc[met.id]['name']
+                #dropping compartment
+                met_id = met.id.split('@')[0]
+                met.name = chem_prop.loc[met_id]['name']
                 try:
-                    met.formula = Formula(chem_prop.loc[met.id].formula)
+                    met.formula = Formula(chem_prop.loc[met_id].formula)
                 except:
-                    logger.debug('Cannot parse formula %s. Skipping formula' % chem_prop.loc[met.id].formula)
+                    logger.debug('Cannot parse formula %s. Skipping formula' % chem_prop.loc[met_id].formula)
                     continue
                 try:
-                    met.charge = int(chem_prop.loc[met.id].charge)
+                    met.charge = int(chem_prop.loc[met_id].charge)
                 except (ValueError, TypeError):
-                    logger.debug('Cannot parse charge %s. Skipping charge' % chem_prop.loc[met.id].charge)
+                    logger.debug('Cannot parse charge %s. Skipping charge' % chem_prop.loc[met_id].charge)
                     pass
-                rest = chem_prop.loc[met.id].to_dict()
+                rest = chem_prop.loc[met_id].to_dict()
                 met.annotation = dict((key, rest[key]) for key in rest if key in ('mass', 'InChI', 'source'))
             mets = [met.id for met in stoichiometry.keys()]
             if len(mets) != len(set(mets)):
@@ -119,12 +124,13 @@ def construct_universal_model(list_of_db_prefixes, reac_xref, reac_prop, chem_pr
             except (AttributeError, ValueError) as e:
                 logger.debug(str(e))
                 continue
-            if row.Balance:
+            if row.is_balanced:
                 reaction.lower_bound = -1 * reaction.upper_bound
-            reaction.name = row['Source']
+            reaction.name = row['reference']
             row = row.fillna("")
             rest = row.to_dict()
-            reaction.annotation = dict((key, rest[key]) for key in rest if key in ('EC', 'Description'))
+            reaction.annotation = dict((key, rest[key]) for key in rest if key in ('classifs', 'reference'))
+            reaction.annotation['Description'] = reac_xref[reac_xref.XREF==row['reference']].Description.iloc[0]
             reactions.append(reaction)
 
     model = Model('metanetx_universal_model_' + '_'.join(list_of_db_prefixes))
@@ -135,26 +141,19 @@ def construct_universal_model(list_of_db_prefixes, reac_xref, reac_prop, chem_pr
     return model
 
 
+def compress_by_lines(response, filepath):
+    with gzip.open(filepath, 'wt') as f:
+        for line in response.iter_lines():
+            f.write(line.decode('utf-8') + '\n')
+            
+
 def load_metanetx_files():
     """"Update metanetx data."""
     BASE_URL = 'http://www.metanetx.org/cgi-bin/mnxget/mnxref/{}.tsv'
     for filename in ['chem_prop', 'chem_xref', 'reac_prop', 'reac_xref', 'comp_prop', 'comp_xref']:
-        response = requests.get(BASE_URL.format(filename))
         filepath = '../data/metanetx/{}.tsv.gz'.format(filename)
+        response = requests.get(BASE_URL.format(filename))
         compress_by_lines(response, filepath)
-
-
-def compress_by_lines(response, filepath):
-    prev_line = next(response.iter_lines())
-    with gzip.open(filepath, 'wb') as f:
-        for line in response.iter_lines(decode_unicode=response.encoding):
-            if line.startswith('#'):
-                prev_line = line
-                continue
-            if prev_line:
-                f.write(str.encode(prev_line + '\n'))
-                prev_line = None
-            f.write(str.encode(line + '\n'))
 
 
 def add_to_all_mapping(dataframe, mapping):
@@ -184,14 +183,14 @@ if __name__ == '__main__':
         load_metanetx_files()
 
     # load metanetx data
-    chem_xref = read_table('../data/metanetx/chem_xref.tsv.gz', compression='gzip')
-    chem_xref.columns = [name.replace('#', '') for name in chem_xref.columns]
-    reac_xref = read_table('../data/metanetx/reac_xref.tsv.gz', compression='gzip')
-    reac_xref.columns = [name.replace('#', '') for name in reac_xref.columns]
-    reac_prop = read_table('../data/metanetx/reac_prop.tsv.gz', compression='gzip', index_col=0)
-    reac_prop.columns = [name.replace('#', '') for name in reac_prop.columns]
-    chem_prop = read_table('../data/metanetx/chem_prop.tsv.gz', compression='gzip', index_col=0,
-                           names=['name', 'formula', 'charge', 'mass', 'InChI', 'SMILES', 'source'])
+    chem_xref = read_table('../data/metanetx/chem_xref.tsv.gz', comment='#')
+    chem_xref.columns = ['XREF', 'MNX_ID', 'Description']
+    reac_xref = read_table('../data/metanetx/reac_xref.tsv.gz', comment='#')
+    reac_xref.columns = ['XREF', 'MNX_ID', 'Description']
+    reac_prop = read_table('../data/metanetx/reac_prop.tsv.gz', comment='#', index_col=0,
+                          names=['mnx_equation', 'reference', 'classifs', 'is_balanced', 'is_transport'])
+    chem_prop = read_table('../data/metanetx/chem_prop.tsv.gz', comment='#', index_col=0, low_memory=False,
+                           names=['name', 'source', 'formula', 'charge', 'mass', 'InChI', 'InChLKey', 'SMILES'])
 
     # replace NaN with None
     chem_prop = chem_prop.where((notnull(chem_prop)), "")
@@ -202,34 +201,35 @@ if __name__ == '__main__':
     metanetx['all2mnx'] = dict()
     metanetx['bigg2mnx'] = dict()
     metanetx['mnx2bigg'] = dict()
+    
     # Metabolites
     for xref in [chem_xref, reac_xref]:
         add_to_bigg_mapping(xref, metanetx['bigg2mnx'], metanetx['mnx2bigg'])
         add_to_all_mapping(xref, metanetx['all2mnx'])
 
-    with gzip.open('../cameo/data/metanetx.json.gz', 'wt') as f:
-        json.dump(metanetx, f)
+    with gzip.open('../data/metanetx.json.gz', 'w') as f:
+        f.write(json.dumps(metanetx).encode('utf-8'))
 
     # generate universal reaction models
-    db_combinations = [('bigg',), ('rhea',), ('bigg', 'rhea'), ('bigg', 'rhea', 'kegg'),
+    db_combinations = [
+        ('bigg',), ('rhea',), ('bigg', 'rhea'), 
+                       ('bigg', 'rhea', 'kegg'),
                        ('bigg', 'rhea', 'kegg', 'brenda')]
+    
     largest_universal_model = None
     for db_combination in db_combinations:
         universal_model = construct_universal_model(db_combination, reac_xref, reac_prop, chem_prop)
         if largest_universal_model is None:
             largest_universal_model = universal_model
-        else:
-            if len(set(largest_universal_model.metabolites)) < len(set(universal_model.metabolites)):
-                largest_universal_model = universal_model
-        # The following is a hack; uncomment the following
-        from cobra.io.json import _REQUIRED_REACTION_ATTRIBUTES
+        elif len(set(largest_universal_model.metabolites)) < len(set(universal_model.metabolites)):
+            largest_universal_model = universal_model
 
-        _REQUIRED_REACTION_ATTRIBUTES.add('annotation')
-        with open('../cameo/models/universal_models/{model_name}.json'.format(model_name=universal_model.id), 'w') as f:
+        with open('../models/universal_models/{model_name}.json'.format(model_name=universal_model.id), 'w') as f:
             save_json_model(universal_model, f)
 
     metabolite_ids_in_largest_model = [m.id for m in largest_universal_model.metabolites]
     chem_prop_filtered = chem_prop.dropna(subset=['name'])
     chem_prop_filtered = chem_prop_filtered[chem_prop_filtered.index.isin(metabolite_ids_in_largest_model)]
-    with gzip.open('../cameo/data/metanetx_chem_prop.json.gz', 'wt') as f:
+    
+    with gzip.open('../data/metanetx_chem_prop.json.gz', 'wt') as f:
         chem_prop_filtered.to_json(f, force_ascii=True)
